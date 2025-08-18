1ac1ddad744ae927df975b2ec85fab9d131e0792
van9md <van9md@gmail.com>
2025-08-18 15:38:05 +0300

    snippet view,create handlers

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"	
+)
 
+func home(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Hello from Snippetbox!"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Display a form for creating specific snippet..."))
+}
 func main() {
-	fmt.Println("Hello world!")
+	mux:=http.NewServeMux()
+	mux.HandleFunc("/{$}",home)
+	mux.HandleFunc("/snippet/view",snippetView)
+	mux.HandleFunc("/snippet/create",snippetCreate)
+	log.Print("Starting server on :4000")
+	err:=http.ListenAndServe(":4000",mux)
+	log.Fatal(err)
 }
