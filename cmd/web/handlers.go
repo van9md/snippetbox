e77efafe6f4d780dbc904000a2b0cd9570280040
van9md <van9md@gmail.com>
2025-08-20 18:48:56 +0300

    Slog

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,31 +3,28 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	files:=[]string{
+	files := []string{
 		"./ui/html/base.tmpl",
 		"./ui/html/pages/home.tmpl",
 		"./ui/html/partials/nav.tmpl",
 	}
-	ts,err:=template.ParseFiles(files...)
-	if err!=nil{
-		log.Print(err.Error())
-		http.Error(w,"Internal Server Error",http.StatusInternalServerError)
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w,r,err)
 		return
 	}
-	err=ts.ExecuteTemplate(w,"base",nil)
-	if err!=nil{
-		log.Print(err.Error())
-		http.Error(w,"Internal Server Error",http.StatusInternalServerError)
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		app.serverError(w,r,err)
 	}
 }
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -35,10 +32,10 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a form for creating a new snippet..."))
 }
-func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("Save a new snippet..."))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,43 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
+	var cfg config
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.Parse()
+
+	app := &application{
+		logger: logger,
+	}
+
 	mux := http.NewServeMux()
-	fileserver:=http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/",http.StripPrefix("/static",fileserver))
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
-	log.Print("starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	fileserver := http.FileServer(http.Dir(cfg.staticDir))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileserver))
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+	logger.Info("starting server", slog.String("addr", cfg.addr))
+	err := http.ListenAndServe(cfg.addr, mux)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
