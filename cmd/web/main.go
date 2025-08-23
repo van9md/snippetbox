df8f365762d41611f6d9c5046d93990c4a6d2f6e
van9md <van9md@gmail.com>
2025-08-23 14:06:49 +0300

    routers in deticated file, trace in log

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,43 +1,61 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/van9md/snippetbox/internal/models"
 )
 
 type config struct {
 	addr      string
 	staticDir string
+	dsn string
 }
 
 type application struct {
 	logger *slog.Logger
+	cfg config
+	snippets *models.SnippetModel 
 }
 
 func main() {
-	var cfg config
-
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-
-	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
-	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
-	flag.Parse()
-
 	app := &application{
 		logger: logger,
+		cfg: config{},
 	}
 
-	mux := http.NewServeMux()
-	fileserver := http.FileServer(http.Dir(cfg.staticDir))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileserver))
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-	logger.Info("starting server", slog.String("addr", cfg.addr))
-	err := http.ListenAndServe(cfg.addr, mux)
+	flag.StringVar(&app.cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&app.cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.StringVar(&app.cfg.dsn,"dsn","web:pass@/snippetbox?parseTime=true","MySQL datasource name")
+	flag.Parse()
+
+
+	logger.Info("starting server", slog.String("addr", app.cfg.addr))
+
+	db,err:=openDB(app.cfg.dsn)
+	defer db.Close()
+	app.snippets=&models.SnippetModel{DB: db}
+
+	err = http.ListenAndServe(app.cfg.addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string)(*sql.DB,error){
+	db,err:=sql.Open("mysql",dsn)
+	if err!=nil{
+		return nil, err
+	}
+	err=db.Ping()
+	if err!=nil{
+		db.Close()
+		return nil,err
+	}
+	return db,nil
 }
